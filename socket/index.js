9a70c7835c7867dbb6b64e84c94dc03aecf2f907
Abishkar <abishkarchapagain1@gmail.com>
2025-04-18 12:04:35 +0545

    socket inbox solve

--- socket/index.js
+++ socket/index.js
@@ -95,7 +95,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT ||4000 ;
 server.listen(PORT, () => {
   console.log(`🚀 Socket.IO server is running on port ${PORT}`);
 });
