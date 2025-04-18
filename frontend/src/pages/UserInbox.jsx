a042113663e2091931b8554f3a1e90275df3346a
Abishkar <abishkarchapagain1@gmail.com>
2025-04-18 11:56:21 +0545

    socket inbox solve

--- frontend/src/pages/UserInbox.jsx
+++ frontend/src/pages/UserInbox.jsx
@@ -9,7 +9,8 @@ import { useNavigate } from "react-router-dom";
 import { AiOutlineArrowRight, AiOutlineSend } from "react-icons/ai";
 import { TfiGallery } from "react-icons/tfi";
 import styles from "../styles/styles";
-const ENDPOINT = "https://shoe-sphere.onrender.com/";
+const ENDPOINT = window.location.origin;
+
 const socketId = socketIO(ENDPOINT, { transports: ["websocket"] });
 
 const UserInbox = () => {
